545be1af314d4fa3274ee3f1a1a55dc7d63a1a64
jeongseoyi <junghee5341@naver.com>
2021-12-13 17:55:50 +0900

    6일차

--- 레퍼런스 사이트 제작/js/script.js
+++ 레퍼런스 사이트 제작/js/script.js
@@ -17,4 +17,39 @@ $(function(){
 
 
     })
+
+    /* 모바일 네비게이션 */
+    // 햄버거 버튼 클릭 이벤트
+    $('header .open').on('click',function(){
+        $('body').css({'overflow' : 'hidden'})
+        $('header .bg').css({'display': 'block'})
+        $('header nav').addClass('on')
+    })
+
+    // X 버튼 클릭 이벤트  & 여백을 눌러도 변함
+    $('header .close, header .bg').on('click',function(){
+        $('body').css({'overflow' : 'auto'})
+        $('header .bg').css({'display': 'none'})
+        $('header nav').removeClass('on')
+    })
+
+
+    // header scroll 배경 넣기
+    let scrollValue = 0
+    scrollValue = $(document).scrollTop()
+
+    function fixHeader(){
+        // 스크롤 위치가 200px 초과하면 배경 넣어주기
+        if(scrollValue > 200){
+            $('header').addClass('on')
+        }else{
+            $('header').removeClass('on')
+        }
+    }
+
+    // 스크롤 위치 감지 이벤트
+    $(window).on('scroll resize',function(){
+        scrollValue = $(document).scrollTop()
+        fixHeader()
+    })
 })
